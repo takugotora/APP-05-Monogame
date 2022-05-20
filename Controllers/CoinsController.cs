2bd2626729617fb7bd9bf7f8735288e9023c30ad
takugotora <83405975+takugotora@users.noreply.github.com>
2022-05-20 01:32:33 +0100

    Update CoinsController.cs

--- Controllers/CoinsController.cs
+++ Controllers/CoinsController.cs
@@ -1,17 +1,17 @@
-﻿using App05MonoGame.Sprites;
+using App05MonoGame.Helpers;
+using App05MonoGame.Models;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 
 
 namespace App05MonoGame.Controllers
 {
-    /// <summary>
-    /// Could be used for three different coloured coins
-    /// </summary>
     public enum CoinColours
     {
-        copper = 100,
+        Copper = 100,
         Silver = 200,
         Gold = 500
     }
@@ -23,75 +23,107 @@ namespace App05MonoGame.Controllers
     /// </summary>
     /// <authors>
     /// Derek Peacock & Andrei Cruceru
+    /// Modified by Jason Huggins (13/05/2021)
     /// </authors>
-    public class CoinsController : IUpdateableInterface, 
-        IDrawableInterface, ICollideableInterface
+    public class CoinsController
     {
-        private App05Game game;
-
-        private Texture2D copperCoinSheet;
+        private SoundEffect coinEffect;
+        private int coinValue;
+        private double maxTime;
+        private double timer;
+        private AnimatedSprite spriteCoinTemplate;
 
         private readonly List<AnimatedSprite> Coins;        
 
         /// <summary>
-        /// Create a new list of coins with one copper coin
+        /// Constructor; initialises a new list of coins which
+        /// will spawn every 3 seconds and the timer. 
         /// </summary>
-        public CoinsController(App05Game game)
+        public CoinsController()
         {
-            this.game = game;
             Coins = new List<AnimatedSprite>();
-
-            copperCoinSheet = game.Content.Load<Texture2D>("Actors/coin_copper");
-            
-            CreateCoin();
+            maxTime = 3.0;
+            timer = maxTime;
         }
 
         /// <summary>
         /// Create an animated sprite of a copper coin
         /// which could be collected by the player for a score
         /// </summary>
-        public void CreateCoin()
+        public void CreateCoin(GraphicsDevice graphics, Texture2D coinSheet)
         {
-            SoundController.PlaySoundEffect(Sounds.Coins);
-
-            Animation animation = new Animation(
-                game.Graphics, "coin", copperCoinSheet, 8);
+            coinEffect = SoundController.GetSoundEffect("Coin");
+            coinValue = (int)CoinColours.Copper;
+            Animation animation = new Animation("coin", coinSheet, 8);
 
             AnimatedSprite coin = new AnimatedSprite()
             {
                 Animation = animation,
-                Image = animation.FirstFrame,
+                Image = animation.SetMainFrame(graphics),
                 Scale = 2.0f,
                 Position = new Vector2(600, 100),
                 Speed = 0,
             };
 
+            spriteCoinTemplate = coin;
+
             Coins.Add(coin);
         }
 
         /// <summary>
-        /// If the sprite collides with a coin the coin becomes
-        /// invisible and inactive.  A sound is played
+        /// Checks if a coin has collided with a player (i.e. the 
+        /// player walks over it to pick it up) and if it has,
+        /// a sound effect will play and the coin will disappear
+        /// from the screen.
         /// </summary>
-        public void DetectCollision(Sprite sprite)
+        /// <returns>
+        /// The value of the coin to add onto the player's current
+        /// score.
+        /// </returns>
+        public int HasCollided(AnimatedPlayer player)
         {
             foreach (AnimatedSprite coin in Coins)
             {
-                if (coin.HasCollided(sprite) && coin.IsAlive)
+                if (coin.HasCollided(player) && coin.IsAlive)
                 {
-                    SoundController.PlaySoundEffect(Sounds.Coins);
+                    coinEffect.Play();
 
                     coin.IsActive = false;
                     coin.IsAlive = false;
                     coin.IsVisible = false;
+
+                    return coinValue;
                 }
-            }           
+            }
+
+            return 0;
         }
 
+        /// <summary>
+        /// Spawns coins into the game at random positions every 3
+        /// seconds. 
+        /// </summary>
         public void Update(GameTime gameTime)
         {
-            // TODO: create more coins every so often??
-            // or recyle collected coins
+            timer -= gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (timer <= 0)
+            {
+                int x = RandomNumber.Generator.Next(1000) + 100;
+                int y = RandomNumber.Generator.Next(500) + 100;
+
+                AnimatedSprite coin = new AnimatedSprite()
+                {
+                    Animation = spriteCoinTemplate.Animation,
+                    Image = spriteCoinTemplate.Image,
+                    Scale = spriteCoinTemplate.Scale,
+                    Position = new Vector2(x, y),
+                    Speed = 0,
+                };
+
+                Coins.Add(coin);
+                timer = maxTime;
+            }
 
             foreach(AnimatedSprite coin in Coins)
             {
@@ -99,12 +131,21 @@ namespace App05MonoGame.Controllers
             }
         }
 
-        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        public void Draw(SpriteBatch spriteBatch)
         {
             foreach (AnimatedSprite coin in Coins)
             {
-                coin.Draw(spriteBatch, gameTime);
+                coin.Draw(spriteBatch);
             }
+        }
+
+        /// <summary>
+        /// Clears the list of coins, used when the game is
+        /// restarted.
+        /// </summary>
+        public void Clear()
+        {
+            Coins.Clear();
         }
     }
 }
